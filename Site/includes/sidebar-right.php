f348d3b2c1bcb6e40decb8180d176505d2fb1754
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-10-25 10:17:20 -0400

    Add sidebar widget framework

--- Site/includes/sidebar-right.php
+++ Site/includes/sidebar-right.php
@@ -1,1 +1,6 @@
-<?php echo 'Right widgets here'; ?>
+<?php
+require_once __DIR__ . '/../widgets/_registry.php';
+
+echo render_widget_box('online', 10);
+echo render_widget_box('server_status');
+echo render_widget_box('recent_deaths', 8);
