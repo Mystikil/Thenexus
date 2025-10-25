6e649e26f56ddb530b64def23c19fe452cdaeef5
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-10-25 10:06:22 -0400

    Add responsive layout and sidebars

--- Site/includes/header.php
+++ Site/includes/header.php
@@ -23,6 +23,7 @@ if (is_file($themeStylesPath)) {
     <meta name="csrf-token" content="<?php echo sanitize($csrfMetaToken); ?>">
     <title><?php echo sanitize(SITE_TITLE); ?></title>
     <link rel="stylesheet" href="<?php echo sanitize(base_url('assets/css/styles.css')); ?>">
+    <link rel="stylesheet" href="<?php echo sanitize(base_url('assets/css/layout.css')); ?>">
     <?php foreach ($themeCssFiles as $cssHref): ?>
         <link rel="stylesheet" href="<?php echo sanitize($cssHref); ?>">
     <?php endforeach; ?>
