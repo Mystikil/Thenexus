e964f81434ba46d5207cf01e5261448142bacfa9
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-10-27 15:27:03 -0400

    Align diagnostics config signatures

--- src/utils/DiagnosticsConfig.cpp
+++ src/utils/DiagnosticsConfig.cpp
@@ -1,29 +1,35 @@
 #include "utils/DiagnosticsConfig.h"
 
+#include <atomic>
+
 namespace {
 
-        std::atomic_bool g_traceStartupEnabled{false};
-        std::atomic_bool g_sqlTraceEnabled{false};
+std::atomic_bool g_sqlTraceEnabled{false};
+std::atomic_bool g_traceStartupEnabled{false};
 
 } // namespace
 
 namespace diagnostics {
 
-        void setTraceStartupEnabled(bool enabled) {
-                g_traceStartupEnabled.store(enabled, std::memory_order_relaxed);
-        }
+bool isSqlTraceEnabled()
+{
+    return g_sqlTraceEnabled.load(std::memory_order_relaxed);
+}
 
-        bool isTraceStartupEnabled() {
-                return g_traceStartupEnabled.load(std::memory_order_relaxed);
-        }
+void setSqlTraceEnabled(bool enabled)
+{
+    g_sqlTraceEnabled.store(enabled, std::memory_order_relaxed);
+}
 
-        void setSqlTraceEnabled(bool enabled) {
-                g_sqlTraceEnabled.store(enabled, std::memory_order_relaxed);
-        }
+bool isTraceStartupEnabled()
+{
+    return g_traceStartupEnabled.load(std::memory_order_relaxed);
+}
 
-        bool isSqlTraceEnabled() {
-                return g_sqlTraceEnabled.load(std::memory_order_relaxed);
-        }
+void setTraceStartupEnabled(bool enabled)
+{
+    g_traceStartupEnabled.store(enabled, std::memory_order_relaxed);
+}
 
 } // namespace diagnostics
 
