29c93a8f740ba5bc30638dd27ce30882bd315317
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-10-27 11:43:51 -0400

    Relax diagnostics atomics

--- src/common/diagnostics.cpp
+++ src/common/diagnostics.cpp
@@ -10,22 +10,22 @@ std::atomic<bool> traceStartupEnabled{false};
 
 bool isSqlTraceEnabled()
 {
-    return sqlTraceEnabled.load();
+    return sqlTraceEnabled.load(std::memory_order_relaxed);
 }
 
 void setSqlTraceEnabled(bool enabled)
 {
-    sqlTraceEnabled.store(enabled);
+    sqlTraceEnabled.store(enabled, std::memory_order_relaxed);
 }
 
 bool isTraceStartupEnabled()
 {
-    return traceStartupEnabled.load();
+    return traceStartupEnabled.load(std::memory_order_relaxed);
 }
 
 void setTraceStartupEnabled(bool enabled)
 {
-    traceStartupEnabled.store(enabled);
+    traceStartupEnabled.store(enabled, std::memory_order_relaxed);
 }
 
 } // namespace diagnostics
